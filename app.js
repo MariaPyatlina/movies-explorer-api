dae5ed686d1b92c453c27c21b39df29330da85cb
LD\mpavlovna <mpavlovna@yandex-team.ru>
2023-02-28 01:19:18 +0300

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 
